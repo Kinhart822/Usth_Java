f85bb223196be4ab7394fc99b7d00eeb4cfddf90
Phạm Hoàng Anh <155462810+Kinhart822@users.noreply.github.com>
2024-01-24 08:53:07 +0700

    Update the code

--- Retake_2023/Student.java
+++ Retake_2023/Student.java
@@ -6,9 +6,7 @@ class Student extends Person {
     private double midTermMark;
     private double finalMark;
     private double finalCourseMark;
-    public String displayGoodStudents;
-    public String displayAverageStudents;
-
+    
     public Student(int id, String name, double attendanceMark, double midTermMark, double finalMark) {
         super();
         setName(name); 
